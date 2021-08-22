2b303b53662d57647c5b08ad84026a224c39bc36
tanz3 <3222839916@qq.com>
2021-08-22 10:17:18 +0000

    haydn: Drop obsolete settings
    Signed-off-by: tanz3 <3222839916@qq.com>

--- device.mk
+++ device.mk
@@ -31,16 +31,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.lahaina
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.lahaina \
-    libgptutils.lahaina \
-    libz \
-    libcutils
+    android.hardware.boot@1.0-service \
+    bootctrl.lahaina.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
