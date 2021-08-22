17bbf9307ede328cd9b3f87c5477fcfed48a0892
tanz3 <3222839916@qq.com>
2021-08-22 10:19:38 +0000

    haydn: Correct includes
    Signed-off-by: tanz3 <3222839916@qq.com>

--- omni_haydn.mk
+++ omni_haydn.mk
@@ -25,9 +25,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := haydn
